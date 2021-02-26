49d3cc1585ba49c611a2cbcbea7ed0c9afdeb3ed
Ivo Woltring <ivo@ivonet.nl>
2021-02-26 19:47:27 +0100

    fixed node httpd bug

--- images/maven/mvn.sh
+++ images/maven/mvn.sh
@@ -6,3 +6,5 @@ docker run -it \
 
 
 docker run -it --rm  -v $(pwd):/project -v $(echo "$HOME/.m2/repository"):/repository ivonet/maven
+docker run -it --rm  -v $(pwd):/project ivonet/maven
+docker run -it --rm  -v %CD%:/project -v $(echo "$HOME/.m2/repository"):/repository ivonet/maven clean package
