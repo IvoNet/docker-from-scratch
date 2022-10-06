e75e536e1c03b5113889166ad6f90ae1bd18307f
Ivo Woltring <ivo@ivonet.nl>
2022-10-06 15:07:08 +0200

    fix for no database problem

---
+++ back-end/src/main/resources/data.sql
@@ -0,0 +1,1 @@
+INSERT INTO `quote` (`author`, `quote`) VALUES('Anonymous', 'A dirty mind is a joy forever.');
---
+++ back-end/src/main/resources/schema.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS `quote` (
+  `id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `author` varchar(250) NOT NULL,
+  `quote` text  NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
