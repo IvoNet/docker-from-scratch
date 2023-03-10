a50a58a2c5182ba744aaaf4644ba202ec7b14642
Ivo Woltring <ivo@ivonet.nl>
2023-03-10 12:18:54 +0100

    updated maven

--- images/maven/setup/entrypoint.sh
+++ images/maven/setup/entrypoint.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 ## Do your own config stuff here
 
-## this copies our settings.xml to the default place where maven will look for it.
-rm -f /usr/share/java/maven-3/conf/settings.xml >/dev/null
-cp /settings.xml /usr/share/java/maven-3/conf/settings.xml
 
 # Always end with this line...
 exec "$@"
